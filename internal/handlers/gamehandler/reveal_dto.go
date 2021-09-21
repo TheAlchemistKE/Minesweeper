11b66e5171068c8816e60a866652e2e1ab482c8a
Kelyn Njeri <kelynpaul20.kp@gmail.com>
2021-09-22 00:15:49 +0300

    Add Error Handling.

--- internal/handlers/gamehandler/reveal_dto.go
+++ internal/handlers/gamehandler/reveal_dto.go
@@ -1,1 +1,14 @@
 package gamehandler
+
+import "github.com/TheAlchemistKE/Minesweeper/internal/core/domain"
+
+type BodyRevealCell struct {
+	Row uint   `json:"row"`
+	Col uint   `json:"col"`
+}
+
+type ResponseRevealCell domain.Game
+
+func BuildResponseRevealCell(model domain.Game) ResponseRevealCell {
+	return ResponseRevealCell(model)
+}
--- internal/repositories/gamesrepo/memkvs.go
+++ internal/repositories/gamesrepo/memkvs.go
@@ -3,6 +3,7 @@ package gamesrepo
 import (
 	"encoding/json"
 	"errors"
+	"github.com/TheAlchemistKE/Minesweeper/internal/core/domain"
 )
 
 type memkvs struct {
@@ -13,16 +14,27 @@ func NewMemKVS() *memkvs {
 	return &memkvs{kvs: map[string][]byte{}}
 }
 
-func (repo *memkvs) Get(id string) (gamedomain.Game, error) {
+func (repo *memkvs) Get(id string) (domain.Game, error) {
 	if value, ok := repo.kvs[id]; ok {
-		game := gamedomain.Game{}
+		game := domain.Game{}
 		err := json.Unmarshal(value, &game)
 		if err != nil {
-			return gamedomain.Game{}, errors.New("fail to get value from kvs")
+			return domain.Game{}, errors.New(apperrors.Internal, err, "fail to get value from kvs")
 		}
 
 		return game, nil
 	}
 
-	return gamedomain.Game{}, errors.New("game not found in kvs")
+	return domain.Game{}, errors.New(apperrors.NotFound, nil, "game not found in kvs")
+}
+
+func (repo *memkvs) Save(game domain.Game) error {
+	bytes, err := json.Marshal(game)
+	if err != nil {
+		return errors.New(apperrors.InvalidInput, err, "game fails at marshal into json string")
+	}
+
+	repo.kvs[game.ID] = bytes
+
+	return nil
 }
