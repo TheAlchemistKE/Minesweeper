9f86b5ec2d05593a05305c3b1f62ee41652b58c7
Kelyn Njeri <kelynpaul20.kp@gmail.com>
2021-09-22 00:04:56 +0300

    Wrap up domain specific code.

--- internal/core/ports/repositories.go
+++ internal/core/ports/repositories.go
@@ -1,1 +1,8 @@
 package ports
+
+import "github.com/TheAlchemistKE/Minesweeper/internal/core/domain"
+
+type GamesRepository interface {
+	Get(id string) (domain.Game, error)
+	Save(domain.Game) error
+}
--- internal/core/ports/services.go
+++ internal/core/ports/services.go
@@ -1,1 +1,9 @@
+package ports
 
+import "github.com/TheAlchemistKE/Minesweeper/internal/core/domain"
+
+type GamesService interface {
+	Get(id string) (domain.Game, error)
+	Create(name string, size uint, bombs uint) (domain.Game, error)
+	Reveal(id string, row uint, col uint) (domain.Game, error)
+}
